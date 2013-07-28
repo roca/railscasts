34b3948defc3b629f1fe7c633a5220e6771656e2
roca <RomelCampbell@gmail.com>
2013-07-27 20:27:54 -0400

    Create rails 4 episode 401

---
+++ 401/chatter/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatter::Application.config.secret_key_base = '0ab0005af980e37358e53b048541b8c45450527acacb2f3286292ef20078d8dd25aec6720d803f734ca640474f4a77b021607b62bb14cd6399c024b4c8992afc'
